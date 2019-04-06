e0517253df30357fe0a9c0a44af23248f8119440
Tomas Camin <tomas.camin@schibsted.com>
2019-04-06 14:28:58 +0200

    Add sbtuitestbrowser 1.0.5

--- sbtuitestbrowser.rb
+++ sbtuitestbrowser.rb
@@ -1,9 +1,9 @@
 class Sbtuitestbrowser < Formula
   desc "A command line tool to parallelize Apple UI Tests"
   homepage "https://github.com/Subito-it/sbtuitestbrowser"
-  url "https://github.com/Subito-it/homebrew-made/raw/master/archive/sbtuitestbrowser/sbtuitestbrowser-1.0.4.tar.gz"
-  sha256 "5e01b0279853cd306bf910be81f35636410ee51ab45ab6dd3ebf78428f4c387f"
-  version "1.0.4"
+  url "https://github.com/Subito-it/homebrew-made/raw/master/archive/sbtuitestbrowser/sbtuitestbrowser-1.0.5.tar.gz"
+  sha256 "f179039116268e907e599b972327fb4651c082f1a29522d8e32c3f6b4c7b5040"
+  version "1.0.5"
 
   bottle :unneeded
 
