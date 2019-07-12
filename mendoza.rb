95464776114564c2bc44fdcfc0b1cd76b03e0c93
Tomas Camin <tomas.camin@adevinta.com>
2019-07-12 13:10:19 +0200

    Add Mendoza 0.3.7

--- mendoza.rb
+++ mendoza.rb
@@ -1,9 +1,9 @@
 class Mendoza < Formula
   desc "A command line tool to parallelize Apple UI Tests"
   homepage "https://github.com/Subito-it/Mendoza"
-  url "https://github.com/Subito-it/homebrew-made/raw/master/archive/mendoza/mendoza-0.3.6.tar.gz"
-  sha256 "5273c8b0c535bc24050b232c376bd2cac112353ec2570db83de48f9ad7bc028b"
-  version "0.3.6"
+  url "https://github.com/Subito-it/homebrew-made/raw/master/archive/mendoza/mendoza-0.3.7.tar.gz"
+  sha256 "f32b49415cc858e9a4b8db7569d11faad4727f9fb004f65d9c217ebd45000a8f"
+  version "0.3.7"
 
   depends_on "libssh2"
 
