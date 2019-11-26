ad4eb1c1cc4027bbe20d842bc93b88ad3fa8e1e7
Tomas Camin <tomas.camin@adevinta.com>
2019-11-26 23:29:45 +0100

    Add Cachi 0.1.4

--- cachi.rb
+++ cachi.rb
@@ -1,9 +1,9 @@
 class Cachi < Formula
   desc "A web interface to visualize xcodebuild's UI tests results."
   homepage "https://github.com/Subito-it/cachi"
-  url "https://github.com/Subito-it/homebrew-made/raw/master/archive/cachi/cachi-0.1.3.tar.gz"
-  sha256 "9faf619fcd7f947c8091518d05bd8df16bc2cea997c1f1c982d7bbf1ae4dcd1f"
-  version "0.1.3"
+  url "https://github.com/Subito-it/homebrew-made/raw/master/archive/cachi/cachi-0.1.4.tar.gz"
+  sha256 "8609ea455982bb722893a48d3d6b87e1fa7d20a30a54dbef54bc9bf662b795f6"
+  version "0.1.4"
 
   bottle :unneeded
 
