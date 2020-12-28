1e4636e192194c86cc43bf30350fb0be113b056a
Tomas Camin <tomas.camin@adevinta.com>
2020-12-28 17:21:43 +0100

    Add Cachi 12.2.2

--- cachi.rb
+++ cachi.rb
@@ -1,9 +1,9 @@
 class Cachi < Formula
   desc "A web interface to visualize xcodebuild's UI tests results."
   homepage "https://github.com/Subito-it/cachi"
-  url "https://github.com/Subito-it/homebrew-made/raw/master/archive/cachi/cachi-12.2.1.tar.gz"
-  sha256 "fe20bf5975504bce49d69d8cefc0711e11e0d1b820a82c7cc39f22a562490769"
-  version "12.2.1"
+  url "https://github.com/Subito-it/homebrew-made/raw/master/archive/cachi/cachi-12.2.2.tar.gz"
+  sha256 "fd24973f57530a03a0e311bbd780b31010421f2304422810842ad636e22bea15"
+  version "12.2.2"
 
   bottle :unneeded
 
